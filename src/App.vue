53e9f74c3e34318fb223ad1ddc1506e0d402c85b
wq93 <214180117@qq.com>
2017-12-06 17:37:57 +0800

    完成iconfont图标字体的引入&添加leftNav组件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <leftNav></leftNav>
+    <!--操作区域-->
+    <div class="main">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import leftNav from '@/components/common/leftNav'
+
+  export default {
+
+    components: {
+      leftNav
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Microsoft YaHei', 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    color: #2c3e50;
+    height: 100%;
+  }
+
+  .main {
+    float: left;
+    width: 95%;
+    background-color: #EFF2F7;
+    height: 100%;
+    overflow: auto;
+
+  }
 </style>
